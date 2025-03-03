4cf434c6d420cfa9b781a0aa96bbcc6484c74652
Anhdai1702 <153721013+Anhdai1702@users.noreply.github.com>
2025-03-03 19:32:04 +0700

    pod install

---
+++ FitnessApp/Screens/SplashViewController/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  FitnessApp
+//
+//  Created by Phùng Anh Đài  on 3/3/25.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
