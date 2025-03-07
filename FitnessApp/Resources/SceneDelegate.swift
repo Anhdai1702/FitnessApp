c2a97e40c9a6fd07840425c1028d72a8d51743c3
Anhdai1702 <153721013+Anhdai1702@users.noreply.github.com>
2025-03-08 00:19:36 +0700

    add sign up

--- FitnessApp/Resources/SceneDelegate.swift
+++ FitnessApp/Resources/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = LoginViewController()
+        let vc = SplashViewController()
         let navi = UINavigationController(rootViewController: vc)
         
         window.rootViewController = navi
--- FitnessApp/Screens/SplashViewController/SplashViewController.swift
+++ FitnessApp/Screens/SplashViewController/SplashViewController.swift
@@ -14,6 +14,7 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigateToScreen()
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     private func navigateToScreen() {
